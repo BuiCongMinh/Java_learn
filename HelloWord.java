065bcbd2db190bf2ee0d71dd9bf693ce317b64db
Test_Terminal_git <minhvnvnvn1998@gmail.com>
2024-08-08 14:56:11 +0700

    firt commit

---
+++ HelloWord.java
@@ -0,0 +1,9 @@
+package Java_learn;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word with mvn !");
+        System.out.println("Hello Word with mvn !");
+        System.out.println("Hello Word with mvn !");
+    }
+}
